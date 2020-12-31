d173453170d1a4626354d5d6e313dfc95dbd5a4f
hoantran <hoan@bluepego.com>
2020-12-30 23:53:19 -0800

    Blank Main View Controller

---
+++ Holdings/MainViewController.h
@@ -0,0 +1,16 @@
+//
+//  MainViewController.h
+//  Holdings
+//
+//  Created by Hoan Tran on 12/30/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MainViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Holdings/MainViewController.m
@@ -0,0 +1,23 @@
+//
+//  MainViewController.m
+//  Holdings
+//
+//  Created by Hoan Tran on 12/30/20.
+//
+
+#import "MainViewController.h"
+
+@interface MainViewController ()
+
+@end
+
+@implementation MainViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.title = @"Holdings";
+    self.view.backgroundColor = [UIColor cyanColor];
+}
+
+@end
